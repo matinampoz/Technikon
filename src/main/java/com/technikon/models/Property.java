96846635bf6a6acb01b0407120f4c3208bb648d4
Konstantinos Pailas <pailaskonst@gmail.com>
2024-07-31 20:38:10 +0300

    Added column specifiers in property class

--- src/main/java/com/technikon/models/Property.java
+++ src/main/java/com/technikon/models/Property.java
@@ -1,6 +1,7 @@
 package com.technikon.models;
 
 import enums.PropertyType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -20,9 +21,16 @@ import lombok.NoArgsConstructor;
 public class Property {
     @Id
     private long propertyID;
+    
+    @Column(name = "address", nullable = false)
     private String address;
+    
+    @Column(name = "yearOfConstruction", nullable = false)
     private int yearOfConstruction;
+    
+    @Column(name = "propertyType", nullable = false)
     private PropertyType typeOfProperty;
+    
     @ManyToOne
     @JoinColumn(name = "VATnumber", referencedColumnName = "VATnumber")
     private long ownerVAT;
