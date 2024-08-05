f8edd4fba959b75422be14c43c6d5bc347ff0000
zouka <zoukaskon@gmail.com>
2024-08-05 14:54:50 +0300

    feat. Property Repair Service Implementation

--- src/main/java/com/technikon/exceptions/PropertyRepairException.java
+++ src/main/java/com/technikon/exceptions/PropertyRepairException.java
@@ -1,5 +1,12 @@
 package com.technikon.exceptions;
 
-public class PropertyRepairException extends Exception {
+public class PropertyRepairException extends RuntimeException {
 
+    public PropertyRepairException(String message) {
+        super(message);
+    }
+
+    public PropertyRepairException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
