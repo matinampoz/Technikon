a6f104228accdceac6d64dbe52566f7509466913
Konstantinos Pailas <pailaskonst@gmail.com>
2024-08-06 18:42:52 +0300

    Added basic UI users

---
+++ src/main/java/com/technikon/UI/Admin.java
@@ -0,0 +1,43 @@
+package com.technikon.UI;
+
+import com.technikon.models.Property;
+import com.technikon.models.PropertyOwner;
+import com.technikon.models.PropertyRepair;
+import java.util.List;
+import java.util.Scanner;
+
+public class Admin implements User {
+
+    @Override
+    public PropertyOwner createNewOwner() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Property createNewProperty() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public PropertyRepair createNewPropertyRepair() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Property> getProperties() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<PropertyRepair> getRepairs() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getAction() {
+        Scanner scanner = new Scanner(System.in);
+        return 0;
+        
+    }
+    
+}
---
+++ src/main/java/com/technikon/UI/Owner.java
@@ -0,0 +1,73 @@
+package com.technikon.UI;
+
+import com.technikon.models.Property;
+import com.technikon.models.PropertyOwner;
+import com.technikon.models.PropertyRepair;
+import enums.RepairStatus;
+import java.util.List;
+import java.util.Scanner;
+
+public class Owner implements User {
+
+    @Override
+    public PropertyOwner createNewOwner() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Property createNewProperty() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public PropertyRepair createNewPropertyRepair() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Property> getProperties() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<PropertyRepair> getRepairs() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    public List<PropertyRepair> getRepairs(RepairStatus status){
+        
+        return null;
+        
+    }
+
+    @Override
+    public int getAction() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                           What would you like to do?
+                           1) Display all my Properties.
+                           2) Display Pending Repairs.
+                           """);
+        
+        return 0;
+    }
+    
+    public int signIn(){
+         Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                           What would you like to do?
+                           1) Create a new profile as a Property Owner.
+                           2) Log In as an already existing Property Owner.
+                           """);
+        
+//        switch (var) {
+//            case val:
+//                
+//                break;
+//            default:
+//                throw new AssertionError();
+//        }
+        return 0;
+    }
+
+}
